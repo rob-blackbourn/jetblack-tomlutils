42f790392f1564eeab33deef743e2aff55af0b98
Rob Blackbourn <rblackbourn@bhdgsystematic.com>
2019-11-27 08:56:12 +0000

    Added json pointer

--- README.md
+++ README.md
@@ -1,24 +1,91 @@
-# bhdg-toml
+# jetblack-tomlutils
 
 Some utilities for working with toml files.
 
 ## Usage
 
-To convert toml to json:
+Install with pip:
 
 ```bash
-$ toml2json < pyproject.toml > pyproject.json
-$ toml2json pyproject.toml > pyproject.json
-$ toml2json pyproject.toml - > pyproject.json
-$ toml2json pyproject.toml pyproject.json
-$ cat pyproject.toml | toml2json
-$ cat pyproject.toml | toml2json -
+$ pip install jetblack-tomlutils
 ```
-To convert json to toml:
+
+### toml2json
+
+To convert toml to JSON:
 
 ```bash
-$ json2toml < pyproject.json
-$ json2toml pyproject.json
-$ cat pyproject.toml | json2toml
-$ cat pyproject.toml | json2toml -
+usage:
+    tom2json [<input> [<output]]
+
+    input/output: either a path or '-' for stdin/stdout
+
+examples:
+    $ toml2json < pyproject.toml > pyproject.json
+    $ toml2json pyproject.toml > pyproject.json
+    $ toml2json pyproject.toml - > pyproject.json
+    $ toml2json pyproject.toml pyproject.json
+    $ cat pyproject.toml | toml2json
+    $ cat pyproject.toml | toml2json -
 ```
+
+### json2toml
+
+To convert JSON to toml:
+
+```bash
+usage:
+    json2toml [<input> [<output]]
+
+    input/output: either a path or '-' for stdin/stdout
+
+examples:
+    $ json2toml < pyproject.json
+    $ json2toml pyproject.json
+    $ cat pyproject.toml | json2toml
+    $ cat pyproject.toml | json2toml -
+```
+
+### jsonget
+
+To query JSON
+
+You can query JSON using a [JSON Pointer](https://tools.ietf.org/html/rfc6901)
+
+```bash
+usage:
+    jsonget <json-pointer> [<input> [<output>]]
+
+    json-pointer: a valid JSON Pointer path
+    input/output: either a path or '-' for stdin/stdout
+
+examples:
+    $ toml2json pyproject.toml | jsonget /tool/poetry/version
+```
+
+### jsonset
+
+To update JSON
+
+You can update JSON using a [JSON Pointer](https://tools.ietf.org/html/rfc6901)
+and a value
+
+```bash
+usage:
+    jsonset <json-pointer> <json-value> [<input> [<output>]]
+
+    json-pointer: a valid JSON Pointer path
+    json-value: a value that can be parsed as JSON.
+    input/output: either a path or '-' for stdin/stdout
+
+
+examples:
+    $ toml2json pyproject.toml | jsonset /tool/poetry/version '"1.2.3"'
+```
+
+## Acknowledgements
+
+This project is a trivial wrapper around the following projects:
+
+* [qtoml](https://github.com/alethiophile/qtoml) - a toml parser
+* [jsonpointer](https://github.com/stefankoegl/python-json-pointer) - a JSON pointer package
--- jetblack_tomlutils/toml_json.py
+++ jetblack_tomlutils/toml_json.py
@@ -5,7 +5,9 @@ import json
 import sys
 from typing import Any, Dict
 
+from jsonpointer import resolve_pointer, set_pointer
 import qtoml
+
 
 class _TomlDecoder(json.JSONEncoder):
     def default(self, obj): # pylint: disable=arguments-differ,method-hidden
@@ -30,31 +32,68 @@ def _load_json(filename: str) -> Dict[str, Any]:
         return json.load(sys.stdin, object_hook=_as_datetime)
     with open(filename, 'rt') as file_ptr:
         return json.load(file_ptr, object_hook=_as_datetime)
-        
 
-def _to_json(input_filename: str = '-', output_filename: str = '-'):
-    content = _load_toml(input_filename)
+
+def _save_json(content: Any, output_filename: str = '-'):
     if output_filename == '-':
         json.dump(content, sys.stdout, cls=_TomlDecoder)
     else:
         with open(output_filename, 'wt') as file_ptr:
             json.dump(content, file_ptr, cls=_TomlDecoder)
 
-def _from_json(input_filename: str = '-', output_filename: str = '-'):
-    content = _load_json(input_filename)
+def _to_json(input_filename: str = '-', output_filename: str = '-'):
+    content = _load_toml(input_filename)
+    _save_json(content, output_filename)
+
+def _save_toml(content: Any, output_filename: str = '-'):
     if output_filename == '-':
         qtoml.dump(content, sys.stdout)
     else:
         with open(output_filename, 'wt') as file_ptr:
             qtoml.dump(content, file_ptr, cls=_TomlDecoder)
 
+def _from_json(input_filename: str = '-', output_filename: str = '-'):
+    content = _load_json(input_filename)
+    _save_toml(content, output_filename)
+
+def _json_get(query: str, input_filename: str = '-', output_filename: str = '-') -> None:
+    doc = _load_json(input_filename)
+    result = resolve_pointer(doc, query)
+    _save_json(result, output_filename)
+
+def _json_set(query: str, value: str, input_filename: str = '-', output_filename: str = '-') -> None:
+    doc = _load_json(input_filename)
+    value = json.loads(value)
+    result = set_pointer(doc, query, value)
+    _save_json(result, output_filename)
+
+def jsonget():
+    """Get JSON using a JSON pointer"""
+    if 1 < len(sys.argv) < 5:
+        _json_get(*sys.argv[1:]) # pylint: disable=no-value-for-parameter
+    else:
+        print(f"""usage:
+    {sys.argv[0]} <jsonpointer> [<input> [<output]]
+
+    input/output: either a path or '-' for stdin/stdout
+""", file=sys.stderr)
+        sys.exit(1)
+
+def jsonset():
+    """Set JSON using a JSON pointer"""
+    if 2 < len(sys.argv) < 6:
+        _json_set(*sys.argv[1:]) # pylint: disable=no-value-for-parameter
+    else:
+        print(f"""usage:
+    {sys.argv[0]} <jsonpointer> <value> [<input> [<output]]
+
+    input/output: either a path or '-' for stdin/stdout
+""", file=sys.stderr)
+        sys.exit(1)
+
 def _convert(func):
-    if len(sys.argv) == 1:
-        func()
-    elif len(sys.argv) == 2:
-        func(sys.argv[1])
-    elif len(sys.argv) == 3:
-        func(sys.argv[1], sys.argv[2])
+    if len(sys.argv) < 3:
+        func(*sys.argv[1:])
     else:
         print(f"""usage:
     {sys.argv[0]} [<input> [<output>]]
@@ -63,7 +102,9 @@ def _convert(func):
 """, file=sys.stderr)
 
 def toml2json():
+    """Convert toml to JSON"""
     _convert(_to_json)
 
 def json2toml():
+    """Convert JSON to toml"""
     _convert(_from_json)
